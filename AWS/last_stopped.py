cae917a68cf5554f2825d0fa53b3c68493805d63
efarjun <89653215+efarjun@users.noreply.github.com>
2022-11-23 05:38:41 -0800

    Update last_stopped.py

--- AWS/last_stopped.py
+++ AWS/last_stopped.py
@@ -15,7 +15,7 @@ ec2Client = boto3.client('ec2', region_name=awsRegion)
 instances = ec2Client.describe_instances()
 
 for reservation in instances['Reservations']:
-        instances = reservation['Instances']
-        for instance in instances:
-            if instance['State']['Name'] == 'stopped':
-                print(f"{instance['InstanceId']}: {instance['StateTransitionReason']}")
+    instances = reservation['Instances']
+    for instance in instances:
+        if instance['State']['Name'] == 'stopped':
+            print(f"{instance['InstanceId']}: {instance['StateTransitionReason']}")
