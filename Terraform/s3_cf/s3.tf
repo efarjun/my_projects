005ff89ef63d62181593289bfa070d0b193520c1
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-26 08:25:11 -0800

    Create s3.tf

---
+++ Terraform/s3_cf/s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning_enabled" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = var.versioning_status
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_test" {
+  bucket = aws_s3_bucket.S3_bucket.id
+  rule {
+      bucket_key_enabled = var.bucket_key
+  }
+}
