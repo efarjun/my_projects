b8741c6ea02aa06f7e4c7b031450404d6533fbd4
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-26 09:11:43 -0800

    Update outputs.tf

--- Terraform/s3_cf/outputs.tf
+++ Terraform/s3_cf/outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_name" {
-  name = aws_s3_bucket.s3_bucket.id
+  value = aws_s3_bucket.s3_bucket.id
 }
 output "cloudfront_distribution_id_with_function" {
-  name = aws_cloudfront_distribution.cloudfront_function_yes.id
+  value = aws_cloudfront_distribution.cloudfront_function_yes.id
 }
 output "cloudfront_distribution_id_no_function" {
-  name = aws_cloudfront_distribution.cloudfront_function_no.id
+  value = aws_cloudfront_distribution.cloudfront_function_no.id
 }
