1b12521d95da1ba1c8152971400f7cfe87b2e40c
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-26 11:03:08 -0800

    Update locals.tf

--- Terraform/s3_cf/locals.tf
+++ Terraform/s3_cf/locals.tf
@@ -1,4 +1,4 @@
 locals {
   s3_origin_id = "s3_origin"
-  cf_function_arn = one(aws_cloudfront_function.cf_function[*].arn)
+  cf_function_arn = one(data.aws_cloudfront_function.cf_function[*].arn)
 }
