9e3870c73eef2af9c436554fece373fecb1756af
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-26 09:01:46 -0800

    Update cloudfront_function.tf

--- Terraform/s3_cf/cloudfront_function.tf
+++ Terraform/s3_cf/cloudfront_function.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_function" "cf_function" {
   count   = var.cf_function == true ? 1 : 0
-  name    = "test"
+  name    = "appendindexhtml"
   runtime = "cloudfront-js-1.0"
   publish = var.cf_function == true ? true : false
   code    = file("${path.module}/cf-function.js")
