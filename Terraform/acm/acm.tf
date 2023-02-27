539fce2221b274679910b3853c066e5363d08ece
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-27 13:12:10 -0800

    Update acm.tf

--- Terraform/acm/acm.tf
+++ Terraform/acm/acm.tf
@@ -2,7 +2,6 @@ resource "aws_acm_certificate" "certificate" {
   domain_name       = var.domain_name
   validation_method = var.validation_method
   subject_alternative_names = var.alternative_names
-  key_algorithm = var.key_algorithm
   options = var.options
   validation_option = var.validation_option
 }
