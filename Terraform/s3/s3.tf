8c369f8c9c4013b078634e520432116cbcfc6939
efarjun <89653215+efarjun@users.noreply.github.com>
2023-02-27 13:23:10 -0800

    Update s3.tf

--- Terraform/s3/s3.tf
+++ Terraform/s3/s3.tf
@@ -1,1 +1,20 @@
 
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = var.acl
+}
+
+resource "aws_s3_bucket_versioning" "versioning_enabled" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = var.versioning_status
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_test" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+      bucket_key_enabled = var.bucket_key
+  }
+}
