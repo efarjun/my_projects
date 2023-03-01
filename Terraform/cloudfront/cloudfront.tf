bdfb5b423317a8cd978544089e2d220c0ba80ad5
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 11:32:12 -0800

    Update cloudfront.tf

--- Terraform/cloudfront/cloudfront.tf
+++ Terraform/cloudfront/cloudfront.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudfront_origin_access_control" "oac" {
   name                              = var.oac_name
   origin_access_control_origin_type = var.oac_type
-  signing_behavior                  = var,signing_behavior
+  signing_behavior                  = var.signing_behavior
   signing_protocol                  = var.signing_protocol
 }
 
