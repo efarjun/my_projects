24e81b3dd19015c2ca8090d10bc264ef370901e5
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 14:03:39 -0800

    Create variables.tf

---
+++ Terraform/cloudfrontfunction/variables.tf
@@ -0,0 +1,12 @@
+variable "function_name" {
+  description = "Name of Cloudfront Function."
+}
+variable "function_runtime" {
+  description = "Function runtime"
+  default = "cloudfront-js-1.0"
+}
+variable "function_publish" {
+  description = "Publish to distribution"
+  type = "bool"
+  default = false
+}
