32bbb569850531c1acc9e45733e2c431c1554be1
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 13:51:16 -0800

    Update s3.tf

--- Terraform/s3/s3.tf
+++ Terraform/s3/s3.tf
@@ -22,5 +22,5 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_acls       = var.public_acls
   block_public_policy     = var.public_policy
   ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets = var.restricted_public_buckets
+  restrict_public_buckets = var.restrict_public_buckets
 }
