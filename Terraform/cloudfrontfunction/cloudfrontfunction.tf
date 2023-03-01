a545e5b72be0eb33cf82ba42369ec3bd0fb89a71
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 14:02:37 -0800

    Update cloudfrontfunction.tf

--- Terraform/cloudfrontfunction/cloudfrontfunction.tf
+++ Terraform/cloudfrontfunction/cloudfrontfunction.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_function" "test" {
   name    = var.function_name
   runtime = var.function_runtime
-  publish = var.publish
+  publish = var.function_publish
   code    = file("${path.module}/$var.function_file}")
 }
