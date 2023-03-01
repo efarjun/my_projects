6c024507f29d7b20610f467538c346ae0055e3a3
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 13:42:56 -0800

    Delete s3_public_access.tf

--- Terraform/s3/s3_public_access.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = var.public_acls
-  block_public_policy     = var.public_policy
-  ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets = var.restricted_public_buckets
-}
