47f35b3c1d8a1dbd264d39bb579fc7152012eb30
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 11:23:46 -0800

    Update cloudfront.tf

--- Terraform/cloudfront/cloudfront.tf
+++ Terraform/cloudfront/cloudfront.tf
@@ -8,10 +8,11 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 resource "aws_cloudfront_origin_access_identity" "oai" {
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
+resource "aws_cloudfront_distribution" "s3_distribution_with_function" {
+  count = var.cf_function == true ? 1 : 0
   origin {
     domain_name              = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
-    origin_id                = local.s3_origin_id
+    origin_id                = var.origin_id
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
@@ -28,8 +29,54 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     function_association {
       event_type   = var.event_type
-      function_arn = aws_cloudfront_function.test.arn
+      function_arn = aws_cloudfront_function.function.arn
     }
+
+    forwarded_values {
+      query_string = var.query_string
+
+      cookies {
+        forward = var.forward
+      }
+    }
+
+    viewer_protocol_policy = var.viewer_protocol_policy
+    min_ttl                = var.min_ttl
+    default_ttl            = var.default_ttl
+    max_ttl                = var.max_ttl
+  }
+
+  price_class = var.price_class
+
+  restrictions {
+    geo_restriction {
+      restriction_type = var.restriction_type
+  }
+}
+  viewer_certificate {
+  cloudfront_default_certificate = var.viewer_certificate
+  ssl_support_method = var.ssl_support_method
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution_without_function" {
+  count = var.cf_function == false ? 1 : 0
+  origin {
+    domain_name              = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    origin_id                = var.origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+  enabled             = var.enabled
+  is_ipv6_enabled     = var.ipv6
+  comment             = var.comment
+  default_root_object = var.root_object
+
+  default_cache_behavior {
+    allowed_methods  = var.allowed_methods
+    cached_methods   = var.cached_methods
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = var.query_string
