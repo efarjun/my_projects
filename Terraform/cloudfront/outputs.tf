66e0d7d653374b16c386be6a2e6c152993f624b5
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-02 05:31:35 -0800

    Update outputs.tf

--- Terraform/cloudfront/outputs.tf
+++ Terraform/cloudfront/outputs.tf
@@ -1,12 +1,12 @@
 output "distribution_id_with_function" {
-  value = aws_cloudfront_distribution.s3_distribution_with_function.id
+  value = one(aws_cloudfront_distribution.s3_distribution_with_function[*].id)
 }
 output"distribution_id_without_function" {
-  value = aws_cloudfront_distribution.s3_distribution_without_function.id
+  value = one(aws_cloudfront_distribution.s3_distribution_without_function[*].id)
 }
 output "distribution_arn_with_function" {
-  value = aws_cloudfront_distribution.s3_distribution_with_function.arn
+  value = one(aws_cloudfront_distribution.s3_distribution_with_function[*].arn)
 }
 output"distribution_arn_without_function" {
-  value = aws_cloudfront_distribution.s3_distribution_without_function.arn
+  value = one(aws_cloudfront_distribution.s3_distribution_without_function[*].arn)
 }
