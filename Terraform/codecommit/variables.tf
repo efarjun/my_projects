6b120675d29b643c47e17d778ba26391009c59d9
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-02 11:24:59 -0800

    Create variables.tf

---
+++ Terraform/codecommit/variables.tf
@@ -0,0 +1,26 @@
+variable "repo_name" {
+    description = "Name of CodeCommit repository."
+}
+variable "description" {
+    description = "Description of repository."
+    default = null
+}
+variable "default_branch" {
+    description = "Default branch of the repository."
+    default = null
+}
+variable "trigger_name" {
+    description = "Name of trigger."
+}
+variable "events" {
+    description = "The repository events that will cause trigger."
+    type = list
+    default = ["all"]
+}
+variable "branches" {
+    description = "The brnaches that will be included in the trigger configuration."
+    default = null
+}
+variable "destination_arn" {
+    description = "The ARN of the resource that is the target for a trigger."
+}
