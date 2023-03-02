5d09f2ace013d4439502eff0327f900d1cc04a6b
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-01 16:27:29 -0800

    Update cloudfront.tf

--- Terraform/cloudfront/cloudfront.tf
+++ Terraform/cloudfront/cloudfront.tf
@@ -11,7 +11,7 @@ resource "aws_cloudfront_origin_access_identity" "oai" {
 resource "aws_cloudfront_distribution" "s3_distribution_with_function" {
   count = var.cf_function == true ? 1 : 0
   origin {
-    domain_name              = var.s3_origin_name
+    domain_name              = "${var.s3_origin_name}.s3.us-east-1.amazonaws.com"
     origin_id                = var.origin_id
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
