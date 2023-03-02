60afd548f45b478ebd37aa9ca2a68c162e4f2d15
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-02 05:02:08 -0800

    Update s3.tf

--- Terraform/s3/s3.tf
+++ Terraform/s3/s3.tf
@@ -7,6 +7,7 @@ resource "aws_s3_bucket_versioning" "versioning_enabled" {
   versioning_configuration {
     status = var.versioning_status
   }
+  depends_on = [aws_s3_bucket.s3_bucket]
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
@@ -14,8 +15,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   rule {
       bucket_key_enabled = var.bucket_key
   }
+  depends_on = [aws_s3_bucket.s3_bucket]
 }
-
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = var.bucket_name
 
@@ -23,4 +24,5 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_policy     = var.public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
+  depends_on = [aws_s3_bucket.s3_bucket]
 }
