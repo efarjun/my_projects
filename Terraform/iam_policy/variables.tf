b2349773f69ab5da632d9f48ffb77bfd8e41fe5a
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-05 10:22:47 -0800

    Create variables.tf

---
+++ Terraform/iam_policy/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of policy."
+}
+variable "path" {
+  description "Name of path."
+  default = null
+}
+variable "description" {
+  description = "Description of policy."
+  default = null
+}
