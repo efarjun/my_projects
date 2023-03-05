c337a60b9d7bcf9cfe88e6931c1e4047619e5086
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-05 09:10:03 -0800

    Update outputs.tf

--- Terraform/codebuild/outputs.tf
+++ Terraform/codebuild/outputs.tf
@@ -1,6 +1,6 @@
 output "codebuild_project_id" {
-  vaule = aws_codebuild_project.codebuild_project.id
+  value = aws_codebuild_project.codebuild_project.id
 }
 output "codebuild_project_arn" {
-  vaule = aws_codebuild_project.codebuild_project.arn
+  value = aws_codebuild_project.codebuild_project.arn
 }
