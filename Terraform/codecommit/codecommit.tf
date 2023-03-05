568b0d4e480e2e7dd485a5ee48baf9992321c506
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-05 08:42:49 -0800

    Update codecommit.tf

--- Terraform/codecommit/codecommit.tf
+++ Terraform/codecommit/codecommit.tf
@@ -3,14 +3,3 @@ resource "aws_codecommit_repository" "code_commit_repo" {
   description     = var.description
   default_branch = var.default_branch
 }
-
-resource "aws_codecommit_trigger" "code_commit_trigger" {
-  repository_name = aws_codecommit_repository.code_commit_repo.repository_name
-
-  trigger {
-    name            = var.trigger_name
-    events          = var.trigger_events
-    branches        = var.branches
-    destination_arn = var.destination_arn
-  }
-}
