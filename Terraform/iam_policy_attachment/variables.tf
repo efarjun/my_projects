4f90c1f8b08d73b2fbcd5858effeaadbb99b6037
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-05 08:56:54 -0800

    Update variables.tf

--- Terraform/iam_policy_attachment/variables.tf
+++ Terraform/iam_policy_attachment/variables.tf
@@ -1,18 +1,21 @@
 variable "name" {
     description = "Name of policy attackment."
 }
+variable "policy_arn" {
+    description = "ARN of resource to apply to policy attachment."
+}
 variable "users" {
     description = "List of users to attach policy to."
+    type = list
     default = null
 }
 variable "groups" {
     description = "List of groups to attach policy to."
+    type = list
     default = null
 }
 variable "roles" {
     description = "List of roles to attach policy to."
+    type = list
     default = null
-}
-variable "policy_arn" {
-    description = "ARN of the policy to attach."
 }
