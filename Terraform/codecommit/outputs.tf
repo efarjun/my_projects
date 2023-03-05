bdccfc57d179accc6166a3c9c373dd1719ce05f4
efarjun <89653215+efarjun@users.noreply.github.com>
2023-03-05 08:42:31 -0800

    Update outputs.tf

--- Terraform/codecommit/outputs.tf
+++ Terraform/codecommit/outputs.tf
@@ -4,6 +4,3 @@ output "repository_id" {
 output "repository_arn" {
   value = aws_codecommit_repository.code_commit_repo.arn
 }
-output "configuration_id" {
-  value = aws_codecommit_trigger.code_commit_trigger.configuration_id
-}
