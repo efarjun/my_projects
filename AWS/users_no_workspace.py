583848a93f911c38b7a254cd70817eacf30532a8
efarjun <89653215+efarjun@users.noreply.github.com>
2023-09-27 16:02:49 -0400

    Create users_no_workspace.py

---
+++ AWS/users_no_workspace.py
@@ -0,0 +1,30 @@
+# The following script iterated through a list of users and returns their username if a workspace exists.
+
+#!/usr/bin/python3
+
+import boto3
+
+# Initialize the AWS Workspaces client
+client = boto3.client('workspaces', region_name='us-east-1')
+
+# List of usernames to check
+usernames = ["user1", "user2"]
+# Directory ID for your AWS Workspaces directory
+directory_id = "d-90674bc264"
+
+# Iterate over the list and get the Workspace ID or an error message
+for username in usernames:
+    try:
+        # Describe the Workspaces for the specified username and directory ID
+        response = client.describe_workspaces(
+            DirectoryId=directory_id,
+            UserName=username
+        )
+
+        # Check if there are Workspaces for the user
+        if len(response['Workspaces']) > 0:
+            workspace_id = response['Workspaces'][0]['WorkspaceId']
+        else:
+            print(f"{username}")
+    except Exception as e:
+        print(f"{username}: Error - {str(e)}")
