54f4326070c9de15364889f89ad527b7ad8fa586
efarjun <89653215+efarjun@users.noreply.github.com>
2026-01-22 09:08:35 -0500

    Create sed_commands.sh

---
+++ Linux/sed_commands.sh
@@ -0,0 +1,5 @@
+find . -type f -exec sed -i 's/demant/birdsong/g' {} +
+
+# .: Tells find to start in the current directory.
+# -type f: Ensures the command only runs on files, not directory names (which would cause errors).
+# {} +: A placeholder that passes the list of found files to sed in batches for better performance. 
